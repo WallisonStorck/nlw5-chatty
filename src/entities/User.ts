d88647f5a348aa60848016ddb9589b5df0d6dc55
wallisonstorck <storck779@gmail.com>
2021-04-22 08:26:56 -0400

    repair error Entity users

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 import { v4 as uuid } from "uuid";
 
-@Entity("user")
+@Entity("users")
 class User {
   @PrimaryColumn()
   id: string;
